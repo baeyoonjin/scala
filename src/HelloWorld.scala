9e7cd466f78033723167c35eab1ae55349e58291
baeyoonjin <byjsj@naver.com>
2014-07-24 22:02:18 +0900

    test

---
+++ src/HelloWorld.scala
@@ -0,0 +1,11 @@
+object HelloWorld {
+  def main (args: Array[String]) {
+//    try {
+//    	println("Hello World")  
+//    } catch {
+////      println("error")
+//    } finally {
+////      println("finally")
+//    }
+  }
+}
